dc88875980608123fca4ce61060369ed75c8b920
Joel Krause <joel@raak.com.au>
2021-03-07 23:11:46 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -140,5 +140,15 @@ module.exports = {
                 },
             })
         })
-    ]
+    ],
+    purge: {
+        enabled: process.env.NODE_ENV === 'production',
+        content: [
+            'components/**/*.vue',
+            'layouts/**/*.vue',
+            'pages/**/*.vue',
+            'plugins/**/*.js',
+            'nuxt.config.js',
+        ]
+    }
 }
